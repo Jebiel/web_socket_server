f7a98af9b7f144407566f6022f6dec631a6fa028
Jess Rasmussen <jess.rasmussen@shortcut.io>
2023-04-21 09:36:39 +0200

    They readyState changes, so the state shouldn't be final.

--- web_socket_connection.dart
+++ web_socket_connection.dart
@@ -7,15 +7,15 @@ class WebSocketConnection {
   WebSocketConnection(this.socket, HttpConnectionInfo _connectionInfo)
       : localPort = _connectionInfo.localPort,
         remotePort = _connectionInfo.remotePort,
-        remoteAddress = _connectionInfo.remoteAddress,
-        state = WebSocketState.fromInt(socket.readyState);
+        remoteAddress = _connectionInfo.remoteAddress;
 
   final WebSocket socket;
-  final WebSocketState state;
 
   final int localPort;
   final int remotePort;
   final InternetAddress remoteAddress;
+
+  WebSocketState get state => WebSocketState.fromInt(socket.readyState);
 
   void send(String message) => socket.add(message);
 
