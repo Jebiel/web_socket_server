e9ebe13552bc220678f304682e783b1b97c7d6a8
Jess <getnet02@yahoo.dk>
2023-04-21 07:45:47 +0200

    Added compression and protocolSelector as optional parameters

--- web_socket_server.dart
+++ web_socket_server.dart
@@ -8,6 +8,9 @@ typedef WebSocketHandler = void Function(WebSocketConnection);
 class WebSocketServer extends Stream<WebSocketConnection> {
   final Duration? pingInterval;
 
+  final CompressionOptions compression;
+  final dynamic Function(List<String>)? protocolSelector;
+
   /// The HttpServer instance that this WebSocketServer is bound to.
   late Future<HttpServer> _httpServer;
 
@@ -22,6 +25,8 @@ class WebSocketServer extends Stream<WebSocketConnection> {
     bool v6Only = false,
     bool shared = false,
     this.pingInterval,
+    this.protocolSelector,
+    this.compression = CompressionOptions.compressionDefault,
   }) : _httpServer = HttpServer.bind(
           address,
           port,
@@ -42,6 +47,8 @@ class WebSocketServer extends Stream<WebSocketConnection> {
     bool shared = false,
     this.pingInterval,
     bool requestClientCertificate = false,
+    this.protocolSelector,
+    this.compression = CompressionOptions.compressionDefault,
   }) : _httpServer = HttpServer.bindSecure(
           address,
           port,
@@ -61,7 +68,11 @@ class WebSocketServer extends Stream<WebSocketConnection> {
       final connectionInfo = request.connectionInfo;
       final isUpgradeRequest = WebSocketTransformer.isUpgradeRequest(request);
       if (isUpgradeRequest && connectionInfo != null) {
-        final webSocket = await WebSocketTransformer.upgrade(request);
+        final webSocket = await WebSocketTransformer.upgrade(
+          request,
+          compression: compression,
+          protocolSelector: protocolSelector,
+        );
         if (pingInterval != null) {
           webSocket.pingInterval = pingInterval!;
         }
