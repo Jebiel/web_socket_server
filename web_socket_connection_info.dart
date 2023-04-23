2d14ce2547bd6cb1f4da68f0dc01cafecc7d87f8
Jess <getnet02@yahoo.dk>
2023-04-23 02:03:14 +0200

    Added WebSocketConnectionInfo class exposing only what's necessary.

---
+++ web_socket_connection_info.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+class WebSocketConnectionInfo {
+  WebSocketConnectionInfo.fromHttpRequest(this._request);
+
+  HttpRequest _request;
+
+  late final uri = _request.uri;
+  late final headers = _request.headers;
+  late final cookies = _request.cookies;
+  late final sessionId = _request.session.id;
+  late final certificate = _request.certificate;
+  late final queryParameters = _request.uri.queryParameters;
+  late final localPort = _request.connectionInfo!.localPort;
+  late final remotePort = _request.connectionInfo!.remotePort;
+  late final remoteAddress = _request.connectionInfo!.remoteAddress;
+}
