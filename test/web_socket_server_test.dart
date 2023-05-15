5d5daac5434947a3fd59f08af5795905beb81d20
Jess Rasmussen <jess.rasmussen@shortcut.io>
2023-05-15 12:42:53 +0200

    Added test, to check that the server can receive a message.
    Added test helper functions for setting up a server and connecting a
    channel afterwards.
    
      1. _testBind
      2. testBind
      3. testBindSecure

--- test/web_socket_server_test.dart
+++ test/web_socket_server_test.dart
@@ -1,13 +1,42 @@
+import 'dart:io';
+
 import 'package:test/test.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:web_socket_server/src/socket.dart';
+import 'package:web_socket_server/web_socket_server.dart';
 
 void main() {
-  group('A group of tests', () {
-    setUp(() {
-      // Additional setup goes here.
-    });
+  test(
+    'Server can receive message from client',
+    () async {
+      final (socket, sink) = await testBind('localhost', 8080);
+      try {
+        sink.add('test');
+        final data = await socket.first;
+        expect('test', equals(data));
+      } finally {
+        await sink.close();
+        await socket.close();
+      }
+    },
+    timeout: Timeout(Duration(seconds: 5)),
+  );
+}
 
-    test('Example test', () {
-      expect(true, isTrue);
-    });
-  });
+Future<(WebSocket, WebSocketSink)> testBind(address, int port) =>
+    _testBind(WebSocketServer.bind(address, port));
+
+Future<(WebSocket, WebSocketSink)> testBindSecure(
+  address,
+  int port,
+  SecurityContext securityContext,
+) =>
+    _testBind(WebSocketServer.bindSecure(address, port, securityContext));
+
+Future<(WebSocket, WebSocketSink)> _testBind(WebSocketServer server) async {
+  late final IOWebSocketChannel channel;
+  final future = server.first.then((socket) => (socket, channel.sink));
+  channel = IOWebSocketChannel.connect(server.url);
+  return future;
 }
